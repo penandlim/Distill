8ba3f472ce5e4a3fba0194272d880d16c80fe3f7
John Lim <penandlim@gmail.com>
2016-10-16 02:36:49 -0700

    do something on click

--- background.js
+++ background.js
@@ -1,6 +1,3 @@
-// Called when the user clicks on the browser action icon.
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.sendMessage(tab.id, {}, function(response) {
-    console.log("Your page has been cleaned up!");
-  });
+    // Do something
 });
