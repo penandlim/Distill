6d61672220e2ea40c192c26604a630e880532f40
John Lim <penandlim@gmail.com>
2016-10-16 02:45:38 -0700

    options page fixed

--- options.js
+++ options.js
@@ -2,8 +2,8 @@ function save_options() {
   var color = document.getElementById('color').value;
   var likesColor = document.getElementById('like').checked;
   chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor
+    "favoriteColor": color,
+    "likesColor": likesColor
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -19,8 +19,8 @@ function save_options() {
 function restore_options() {
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true
+    "favoriteColor": 'red',
+    "likesColor": true
   }, function(items) {
     document.getElementById('color').value = items.favoriteColor;
     document.getElementById('like').checked = items.likesColor;
