6a3f15f3785aa9c0d9866f3dd595ae26e7a1d053
Gerald Collom <geraldcollom@D-173-250-157-201.dhcp4.washington.edu>
2016-10-16 04:35:25 -0700

    added swears and filters

--- options.js
+++ options.js
@@ -1,9 +1,23 @@
 function save_options() {
-  var color = document.getElementById('color').value;
-  var likesColor = document.getElementById('like').checked;
+  var gore = document.getElementById('gore').checked;
+  var bugs = document.getElementById('bugs').checked;
+  var swears = document.getElementById('swears').checked;
+  var nsfw = document.getElementById('nsfw').checked;
+  var selfharm = document.getElementById('self-harm').checked;
+  var drugs = document.getElementById('drugs').checked;
+  var war = document.getElementById('war').checked;
+  var scary = document.getElementById('scary').checked;
+  var suicide = document.getElementById('suicide').checked;
   chrome.storage.sync.set({
-    "favoriteColor": color,
-    "likesColor": likesColor
+    "gore": gore,
+    "bugs": bugs,
+    "swears": swears,
+    "nsfw": nsfw,
+    "selfharm": selfharm,
+    "drugs": drugs,
+    "war": war,
+    "scary": scary,
+    "suicide": suicide
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -19,11 +33,25 @@ function save_options() {
 function restore_options() {
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
-    "favoriteColor": 'red',
-    "likesColor": true
+    "gore": false,
+    "bugs": false,
+    "swears": false,
+    "nsfw": false,
+    "selfharm": false,
+    "drugs": false,
+    "war": false,
+    "scary": false,
+    "suicide": false
   }, function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
+    document.getElementById('gore').checked = items.gore;
+    document.getElementById('bugs').checked = items.bugs;
+    document.getElementById('swears').checked = items.swears;
+    document.getElementById('nsfw').checked = items.nsfw;
+    document.getElementById('self-harm').checked = items.selfharm;
+    document.getElementById('drugs').checked = items.drugs;
+    document.getElementById('war').checked = items.war;
+    document.getElementById('scary').checked = items.scary;
+    document.getElementById('suicide').checked = items.suicide;
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
